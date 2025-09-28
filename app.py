6f42fcc0154bd9291fb113a2c8be421fc70a702a
Rahul khurana <visheshyadavhear@gmail.com>
2025-09-29 00:55:13 +0530

    add some new feature

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request,send_file
+import csv
 app = Flask('__name__')
 
 @app.route('/')
 def main():
     return render_template("index.html")
-print("..")
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email_address = request.form['email_address']
+    subject = request.form['subject']
+    phone = request.form['phone']
+    message = request.form['message']
+
+
+    with open('c_data.csv','a',newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([name, email_address, subject, phone, message])
+    return render_template('index.html')
+
+@app.route('/data')
+def data():
+    return send_file('c_data.csv',as_attachment=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
